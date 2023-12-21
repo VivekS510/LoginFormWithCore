c1dafbd663ce3b2887d8ab2663e24a221d0d45d0
Vivek <svivek1507@gmail.com>
2023-12-21 00:50:11 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using LoginFormWithCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoginFormWithCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly Db_2012Context _dbcontext;
+
+
+        public HomeController(ILogger<HomeController> logger, Db_2012Context dbcontext)
+        {
+            _logger = logger;
+            _dbcontext = dbcontext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                return RedirectToAction("Dashboard");
+            }
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(UsrTbl user)
+        {
+            var myUser = _dbcontext.UsrTbls.Where(x => x.Email == user.Email && x.Password == user.Password).FirstOrDefault();
+            if (myUser != null) 
+            {
+                HttpContext.Session.SetString("UserSession", value: myUser.Email);
+                return RedirectToAction("Dashboard");
+            }
+            else
+            {
+                ViewBag.Message = "LoginFailed";
+            }
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            if(HttpContext.Session.GetString("UserSession") != null) 
+            {
+                ViewBag.MySession = HttpContext.Session.GetString("UserSession").ToString();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            if (HttpContext.Session.GetString("UserSession") != null)
+            {
+                HttpContext.Session.Remove("UserSession");
+                return RedirectToAction("Login"); 
+            }
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoginFormWithCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
